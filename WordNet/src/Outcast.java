6364faa43f3b325d83de347ceff92a3a7fb9ca46
Vladyslav Kiriushkin <vkiriushkin@gmail.com>
2013-03-28 17:06:38 +0200

    WordNet base

---
+++ WordNet/src/Outcast.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: et33027
+ * Date: 3/28/13
+ * Time: 5:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Outcast {
+}
---
+++ WordNet/src/WordNet.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: et33027
+ * Date: 3/28/13
+ * Time: 5:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordNet {
+}
